0151f936f21587238c4f97fb7fe0160adbb970f9
xoto12 <alexandrepribeiro1@gmail.com>
2022-01-17 22:20:40 +0000

    Acesso

--- src/AcessoES.java
+++ src/AcessoES.java
@@ -4,74 +4,44 @@ import java.util.*;
 
 public class AcessoES
 {
-    public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException
-    {
+    public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException {
 
         GestaoAcesso gestaoacesso = new GestaoAcesso();
 
-
         Scanner scanI = new Scanner(System.in);   //Integer scanner
         Scanner scanS = new Scanner(System.in);   //String scanner
-        //Scanner scanD = new Scanner(System.in);   //Double scanner
 
-        Candidato c = null;
-        Curso cur = null;
+        //Candidato c = null;
+        //Curso cur = null;
 
-        final String fileName= "AcessoData.ser";
+        //Map<Integer, Candidato> acesso=null;
+        //List<Curso> acessocurso;
 
-        List<Candidato> acesso;
-
-        int opc1=-1;  //opcao menu1
-        int idd,aux;
-
-
-        //Ler Ficheiro de dados
-        File f = new File(fileName);
-
-        // Check if the specified file
-        // Exists or not
-        if (f.exists())
-        {
-            try(FileInputStream fis= new FileInputStream(fileName))
-            {
-                try(ObjectInputStream ois= new ObjectInputStream(fis))
-                {
-                    acesso = (List<Candidato>)ois.readObject();
-                    for(Candidato can : acesso)
-                        gestaoacesso.addCandidato(can.clone());
-                }
-                catch(IOException e)
-                {
-                    System.out.println("Erro!");
-                }
-            }
-            catch(ClassNotFoundException b){}
-        }
-
-
+        int opc1 = -1, opc2 = -1;  //opcao menu1, menu2
+        int opccurso = -1; //opcao curso
+        int idd=0, idcurso=0;
 
 
         //Menu
-        do
-        {
+        do {
             clearScreen();
             System.out.println("1. Registar Candidato");
             System.out.println("2. Registar Curso");
-            System.out.println("3. ");
-            System.out.println("4. Listar");
+            System.out.println("3. Registar Candidatura");
+            System.out.println("4. Mostrar Resultado da Candidatura");
+            System.out.println("5. Listar");
             System.out.println("0. EXIT");
 
             System.out.print("Escolha a sua opção: ");
             opc1 = scanI.nextInt();
             clearScreen();
 
-            switch(opc1)
-            {
-
+            switch (opc1) {
 
                 case 1:
                     clearScreen();
                     System.out.println("** REGISTRO DE UM NOVO ALUNO **");
+                    idd++;
 
                     System.out.print("Nome: ");
                     String cNome = scanS.nextLine();
@@ -91,34 +61,22 @@ public class AcessoES
                     System.out.print("Media Secundário: ");
                     int cNotaSec = scanI.nextInt();
 
-                    /*HashMap<String, Curso> listacursos = new HashMap<>();
-                    int i= 0;
-                    while(i<5){
-                        int curso = scanI.nextInt();
-                        listacursos.put("", curso);
-                    }*/
-
                     System.out.print("Aluno/a de Regiao Desfavorecida? ** 1-SIM || 2-NÃO **");
                     int cReg = scanS.nextInt();
 
-                    idd = 0;
-
-                    if(cReg == 1)
-                    {
+                    if (cReg == 1) {
                         System.out.print("Introduza o código da Regiao Desfavorecida: ");
                         int cRegCode = scanI.nextInt();
 
                         AlunoRegioes areg = new AlunoRegioes(cNome, cGenero, cNotaA, cNotaB, cNotaIng, cNotaSec, cRegCode);
-                        idd = areg.id;
-                        gestaoacesso.addCandidato(areg.clone());
-
+                        idd = areg.getID();
+                        gestaoacesso.novoCandidato(idd, areg.clone());
                     }
 
                     System.out.print("Aluno/a com necessidades especiais? ** 1-SIM || 2-NÃO **");
                     int cNesEsp = scanS.nextInt();
 
-                    if(cNesEsp == 1)
-                    {
+                    if (cNesEsp == 1) {
                         System.out.print("Introduza o nível de necessidade: ");
                         int cNivelNecessidade = scanI.nextInt();
 
@@ -126,16 +84,14 @@ public class AcessoES
                         String cTipoIncap = scanS.nextLine();
 
                         AlunoEspeciais ae = new AlunoEspeciais(cNome, cGenero, cNotaA, cNotaB, cNotaIng, cNotaSec, cNivelNecessidade, cTipoIncap);
-                        idd = ae.id;
-                        gestaoacesso.addCandidato(ae.clone());
+                        idd = ae.getID();
+                        gestaoacesso.novoCandidato(idd, ae.clone());
                     }
 
                     AlunoRegular ar = new AlunoRegular(cNome, cGenero, cNotaA, cNotaB, cNotaIng, cNotaSec);
-                    idd = ar.id;
-                    gestaoacesso.addCandidato(ar.clone());
-
+                    idd = ar.getID();
+                    gestaoacesso.novoCandidato(idd,ar.clone());
                     break;
-
 
                 case 2:
                     clearScreen();
@@ -150,79 +106,89 @@ public class AcessoES
                     System.out.print("Numerus Clausus: ");
                     int NumClaus = scanI.nextInt();
 
+                    System.out.println("Escolha o tipo de curso: ");
 
+                    System.out.println("1 - BioCiências");
+                    System.out.println("2 - Ciências");
+                    System.out.println("3 - Ciências Jurídicas");
+                    System.out.println("4 - Engenharias");
+                    System.out.println("5 - Humanidades");
 
+                    opccurso = scanI.nextInt();
 
-                    break;
+                    if (opccurso == 1) {
+                        Biociencias bio = new Biociencias(NomeUni, NomeCurso, NumClaus);
+                        gestaoacesso.novoCurso(idcurso, bio.clone());
+                        break;
+                    }
+                    if (opccurso == 2) {
+                        Ciencias cie = new Ciencias(NomeUni, NomeCurso, NumClaus);
+                        gestaoacesso.novoCurso(idcurso, cie.clone());
+                        break;
+                    }
+                    if (opccurso == 3) {
+                        CJuridicas juri = new CJuridicas(NomeUni, NomeCurso, NumClaus);
+                        gestaoacesso.novoCurso(idcurso, juri.clone());
+                        break;
+                    }
+                    if (opccurso == 4) {
+                        Engenharia eng = new Engenharia(NomeUni, NomeCurso, NumClaus);
+                        gestaoacesso.novoCurso(idcurso, eng.clone());
+                        break;
+                    }
+                    if (opccurso == 5) {
+                        Humanidades hum = new Humanidades(NomeUni, NomeCurso, NumClaus);
+                        gestaoacesso.novoCurso(idcurso, hum.clone());
+                        break;
+                    }
 
                 case 3:
-                    System.out.print("cheguei aqui ");
-                    TreeSet<Candidato>temp = new TreeSet<Candidato>();
-                    temp = gestaoacesso.candidatura();
-                    for(Candidato c1: temp){
-                        System.out.println(temp);
-                    }
 
-                    break;
+                   break;
 
                 case 4:
-                    System.out.println("Id: ");
-                    aux = scanI.nextInt();
+                    break;
 
-                    boolean check = false;
-                    for(Candidato c1: GestaoAcesso.getCandidato()){
-                        if(aux == c1.id){
-                            check = true;
-                            System.out.println('\n'+ c1.toString() +'\n');
-                        }else{
-                            check = false;
-                            System.out.println("Id inválido!! ");
+                case 5:
+                    do {
+                        clearScreen();
+                        System.out.println("1. Listar Colocados");
+                        System.out.println("2. Mostar Curso com média mais alta");
+                        System.out.println("3. Mostrar Curso com média mais baixa");
+                        System.out.println("4. Listar alunos colocados 1ªopção");
+                        System.out.println("5. Listar curso colocados 1ªopção");
+                        System.out.println("0. Voltar ao Menu Anterior");
+
+                        System.out.print("Escolha a sua opção: ");
+                        opc2 = scanI.nextInt();
+                        clearScreen();
+
+                        switch (opc2) {
+                            case 1:
+                                break;
+
+                            case 2:
+                                break;
+
                         }
-                    }
+
+
+                    } while (opc2 != 5);
+
                     break;
 
                 case 0:
                     System.out.print("Obrigado por usar a nossa aplicação!!\n");
-
-                    try
-                    {
-                        char x = (char)System.in.read();
-                    }
-                    catch(IOException ioEx)
-                    {
-                        System.out.println("Nao conseguiu ler");
-                    }
-                    clearScreen();
-
-                    try(FileOutputStream fos= new FileOutputStream(fileName))
-                    {
-                        char x = (char)System.in.read();
-                        ObjectOutputStream oos= new ObjectOutputStream(fos);
-                        oos.writeObject(GestaoAcesso.getCandidato());
-                        oos.close();
-                    }
-                    catch(FileNotFoundException fEx)
-                    {
-                        System.out.println("Ficheiro não encontrado!");
-                    }
-
-
-                    finally
-                    {
-                        System.exit(0);
-                    }
-
-                    clearScreen();
+                    gestaoacesso.guardaFicheiro();
 
                     break;
 
-                default: System.out.println("Escolha uma opção válida!\n");
+                default:
+                    System.out.println("Escolha uma opção válida!\n");
+
             }
 
-
-
         }while(opc1 != 0);
-
     }
 
 
@@ -238,8 +204,6 @@ public class AcessoES
         }
         catch (Exception ex) {}
     }
-
-
 
 
 }
--- src/GestaoAcesso.java
+++ src/GestaoAcesso.java
@@ -1,104 +1,128 @@
+import java.io.*;
 import java.util.*;
 
 public class GestaoAcesso
 {
-    public static ArrayList<Candidato> candidato;
-    public static ArrayList<Curso> curso;
+    //private static ArrayList<Candidato> candidato;
+    private HashMap<Integer, Candidato> listaCandidatos = new HashMap<>();
+    private HashMap<Integer, Curso> listaCursos = new HashMap<>();
+    //private static ArrayList<Curso> curso;
+    //private ArrayList<> ListaCjuridicas;
 
 
-    static
+    public GestaoAcesso()
     {
-        candidato = new ArrayList<Candidato>();
-        curso = new ArrayList<Curso>();
+        //this.candidato = new ArrayList<Candidato>();
+        this.listaCandidatos= new HashMap<Integer, Candidato>();
+        this.listaCursos = new HashMap<Integer, Curso>();
+        //this.curso = new ArrayList<Curso>();
+        //this.ListaCjuridicas = new ArrayList<Curso>();
     }
 
-    public static List<Candidato> getCandidato() { return candidato; }
-    public static List<Curso> getCurso() { return curso; }
+    //public static List<Candidato> getCandidato() { return candidato; }
+    //public static List<Curso> getCurso() { return curso; }
 
-    //Adiciona Candidatos 
+    /*Adiciona Candidatos
     public void addCandidato(Candidato c)
     {
         if(c != null)
             candidato.add(c.clone());
     }
 
-    public void addCurso(Curso c)
+    //Adiciona Cursos
+    public void addCurso(Curso cur)
     {
-        if(c != null)
-            curso.add(c.clone());
+        if(cur != null)
+            curso.add(cur.clone());
+    }*/
+
+    public void novoCandidato(int id, Candidato novo){
+        this.listaCandidatos.put(id, novo.clone());
+    }
+
+    public void novoCurso(int id, Curso novo){
+        this.listaCursos.put(id, novo.clone());
     }
 
 
-    public void listarCandidatos(){
 
-        for(Candidato c: candidato){
-            System.out.println(candidato);
+
+    public void lerFicheiro(){
+
+        //Ler Ficheiro de dados dos candidatos
+        try{
+            File f = new File("candidatos.ser");
+            if (!f.exists())
+                return;
+            FileInputStream fis = new FileInputStream(f);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            listaCandidatos =(HashMap) ois.readObject();
+            ois.close();
+            fis.close();
+        }catch (IOException E) {
+            System.out.println("Erro na leitura dos dados dos candidatos!");
+            E.printStackTrace();
+        }catch (ClassNotFoundException C) {
+            System.out.println("Classe não encontrada.");
+            C.printStackTrace();
         }
-    }
 
-    public void listarCursos(){
+        //Ler Ficheiro de dados dos cursos
+        try{
+            File fC = new File("cursos.ser");
+            if (!fC.exists())
+                return;
 
-        for(Curso c: curso){
-            System.out.println(curso);
+            ObjectInputStream ois2 = new ObjectInputStream(new FileInputStream(fC));
+            listaCursos = (HashMap) ois2.readObject();
+            ois2.close();
+
+        }catch (IOException E) {
+            System.out.println("Erro na leitura dos dados dos cursos!");
+            E.printStackTrace();
+        }catch (ClassNotFoundException C) {
+            System.out.println("Classe não encontrada.");
+            C.printStackTrace();
         }
+
     }
 
-    public int NTotalCandidatos(){
-        int temp = 0;
-
-        for(Candidato i: candidato){
-            temp++;
-        }
-        return temp;
-    }
-
-    public int NTotalCursos(){
-        int temp = 0;
-
-        for(Curso c: curso){
-            temp++;
-        }
-        return temp;
-    }
-
-    public TreeSet<Candidato> verificaMedia(Candidato candidato, Curso curso){
-        double min = -1.0;
-
-        TreeSet<Candidato> temp = new TreeSet<Candidato>();
-
-        Candidato temp1 = new Candidato() {
-            @Override
-            public Candidato clone() {
-                return null;
+    public void guardaFicheiro(){
+        try{
+            File ff = new File("candidatos.ser");
+            if (!ff.exists()){
+                ff.createNewFile();
             }
-        };
+            FileOutputStream fos = new FileOutputStream(ff);
+            ObjectOutputStream out = new ObjectOutputStream(fos);
+            out.writeObject(listaCandidatos);
+            out.flush();
+            out.close();
+            fos.close();
+        }catch (IOException E){
+            System.out.println("Erro no Output dos dados de candidato.");
+        }
 
-        for(Candidato i: curso.colocados()){
-            if(curso.calcmedia(candidato) < min){
-                min = curso.calcmedia(i);
-                temp1 = i;
+
+        try{
+            File ffC = new File("cursos.ser");
+            if (!ffC.exists()){
+                ffC.createNewFile();
             }
+            FileOutputStream fos2 = new FileOutputStream(ffC);
+            ObjectOutputStream out = new ObjectOutputStream(fos2);
+            out.writeObject(listaCursos);
+            out.flush();
+            out.close();
+            fos2.close();
+        }catch (IOException E){
+            System.out.println("Erro no Output dos dados dos cursos.");
         }
-
-        if(curso.calcmedia(candidato) > min){
-            curso.colocados().remove(temp1);
-            curso.colocados().add(candidato);
-        }
-        temp = curso.colocados();
-
-        return temp;
     }
 
 
-    public TreeSet<Candidato> candidatura(){
-        TreeSet<Candidato>temp = new TreeSet<Candidato>();
-        for(Candidato c: candidato){
-            for(Curso cc: c.getListaCursos()){
-                temp = verificaMedia(c,cc);
-            }
-        }
-        return temp;
-    }
+
+
 
 
 }
